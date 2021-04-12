36a6ccd9083cfe0177f54c99f0a699febdf728db
nestor <nestoragbangla0027@gmail.com>
2021-04-12 12:07:34 +0100

    first commit

---
+++ js/template.js
@@ -0,0 +1,106 @@
+
+$('.menuHorizon').html('<nav class="navbar navbar-expand-lg navbar-light">' +
+                        '<a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="#" /></a>' +
+                        '<button class="navbar-toggler nav-custome1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
+                            '<span class="navbar-toggler-icon"></span>' +
+                        '</button>' +
+                        '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
+                            '<ul class="navbar-nav ml-auto">' +
+                               ' <li class="nav-item dropdown">' +
+                                    '<a class="nav-link" href="index.html" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">' +
+                                       ' Accueil <!--<i class="fas fa-plus"></i>-->' +
+                                    '</a>' +
+                                '</li>' +
+                                '<li class="nav-item dropdown">' +
+                                    '<a class="nav-link" href="prestations.html" id="navbarDropdown1" role="button" aria-haspopup="true" aria-expanded="false">' +
+                                        'Nos prestations' +
+                                    '</a>' +
+                                '</li>' +
+                                '<li class="nav-item dropdown">' +
+                                    '<a class="nav-link" href="collections.html" id="navbarDropdown3" role="button" aria-haspopup="true" aria-expanded="false"> ' +
+                                       ' Nos collections <!--<i class="fas fa-plus"></i>-->' +
+                                    '</a>' +
+                               ' </li>' +
+                                '<li class="nav-item dropdown">' +
+                                    '<a class="nav-link" href="accessoires.html" id="navbarDropdown3" role="button" aria-haspopup="true" aria-expanded="false"> ' +
+                                        'Nos accessoires <!--<i class="fas fa-plus"></i>-->' +
+                                    '</a>' +
+                                '</li>' +
+                                
+                                
+                                '<li class="nav-item dropdown">' +
+                                    '<a class="nav-link" href="contact.html" id="navbarDropdown4" role="button" aria-haspopup="true" aria-expanded="false">' +
+                                        'Contact' +
+                                    '</a>' +
+                               ' </li>' +
+                            '</ul>' +
+                        '</div>' +
+                    '</nav>');
+
+$('footer').html('<div class="container container-custom">' +
+        '    <div class="row">' +
+        '        <div class="col-sm-12 col-md-12 col-lg-5">' +
+        '            <div class="foot-contact-block">' +
+        '                <img src="images/foot-logo.png" class="img-fluid" alt="#" />' +
+        '                <a href="tel:22960809191">' +
+        '                    <h4><i class="fas fa-phone"></i>(229)60809191</h4>' +
+        '                </a>' +
+        '                <a href="mailto:contact@horizonoptique.com">' +
+        '                    <h4><i class="far fa-envelope"></i>contact@horizonoptique.com</h4>' +
+        '                </a>' +
+        '                <a href="tel:22995710522">' +
+        '                    <h4><i class="fab fa-whatsapp"></i>(229)95710522</h4>' +
+        '                </a>' +
+        '                <a href="">' +
+        '                    <h4><i class="fa fa-location-arrow"></i>Agontikon à coté de l\'ECOBANK</h4>' +
+        '                </a>' +
+        '            </div>' +
+              '  </div>' +
+        '        <div class="col-sm-6 col-md-4 col-lg-3 offset-lg-1">' +
+         '           <div class="foot-link-box">' +
+        '                <h4>Plan du Site</h4>' +
+        '                <ul>' +
+        '                    <li>' +
+        '                        <a href="prestations.html"><i class="fas fa-angle-double-right"></i>Nos Prestations</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="collections.html"><i class="fas fa-angle-double-right"></i>Nos Collections</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="accessoires.html"><i class="fas fa-angle-double-right"></i>Nos accessoires</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="contact.html"><i class="fas fa-angle-double-right"></i>Contact</a>' +
+        '                    </li>' +
+        '                </ul>' +
+        '            </div>' +
+        '        </div>' +
+         '       <div class="col-sm-6 col-md-4 col-lg-3">' +
+        '            <div class="foot-link-box">' +
+        '                <h4>Nos Prestations</h4>' +
+        '                <ul>' +
+        '                    <li>' +
+        '                        <a href="prestations.html"><i class="fas fa-angle-double-right"></i>Entretien</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="prestations.html"><i class="fas fa-angle-double-right"></i>Traitement</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="prestations.html"><i class="fas fa-angle-double-right"></i>Monture</a>' +
+        '                    </li>' +
+        '                    <li>' +
+        '                        <a href="prestations.html"><i class="fas fa-angle-double-right"></i>Verres</a>' +
+        '                    </li>' +
+        '                </ul>' +
+        '            </div>' +
+        '        </div>' +
+        '    </div>' +
+        '    <div class="row" style="text-align: center;">' +
+        '        <div class="col-md-12">' +
+        '            <div class="copyright">' +
+        '                <p>Copyright © 2020 HORIZON OPTIQUE | Réalisé par <a href="https://easycom-dev.com">EasyCom Solutions</a></p>' +
+        '                <a href="#" id="scroll"><i class="fas fa-angle-double-up"></i></a>' +
+        '            </div>' +
+        '        </div>' +
+        '    </div>' +
+        '</div>');
